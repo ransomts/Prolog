c4e663f0852daa8042c10894e812788769eab141
Tim Ransom <ransomts@appstate.edu>
2016-03-17 22:38:00 -0400

    Problem 15 solution
    Look back on this one later...

--- 99 Problems/lists.pl
+++ 99 Problems/lists.pl
@@ -75,7 +75,7 @@ compress([A, B | Cs], [A | Zs]) :- A \= B, compress([B | Cs], Zs).
 pack([], []).
 pack([A], [A]).
 % list with many of one element
-pack([A, B | Bs], [[A, A | As] | Cs]).
+%pack([A, B | Bs], [[A, A | As] | Cs]).
 
 
 %% P14 (*) Duplicate the elements of a list.
@@ -85,3 +85,20 @@ pack([A, B | Bs], [[A, A | As] | Cs]).
 dupli([], []).
 dupli([X], [X, X]).
 dupli([X | Xs], Ys) :- dupli([X], A), dupli(Xs, As), append(A, As, Ys).
+
+
+% P15 (**): Duplicate the elements of a list agiven number of times
+
+% dupli(L1,N,L2) :- L2 is obtained from L1 by duplicating all elements
+%    N times.
+%    (list,integer,list) (?,+,?)
+
+dupli(L1,N,L2) :- dupli(L1,N,L2,N).
+
+% dupli(L1,N,L2,K) :- L2 is obtained from L1 by duplicating its leading
+%    element K times, all other elements N times.
+%    (list,integer,list,integer) (?,+,?,+)
+
+dupli([],_,[],_).
+dupli([_|Xs],N,   Ys, 0) :- dupli(Xs,N,Ys,N).
+dupli([X|Xs],N,[X|Ys],K) :- K > 0, K1 is K - 1, dupli([X|Xs],N,Ys,K1).
